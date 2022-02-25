d4a2d9d0a6cf8228e8ec902b4ec23938845c6577
Ben Lirio <benjlirio@gmail.com>
2022-02-24 20:18:04 -0500

    Lattice gui

---
+++ p5/public/lattice.js
@@ -0,0 +1,23 @@
+drawBasis = () => {
+  state.basis.forEach( ({x,y}) => {
+    stroke('blue')
+    strokeWeight(2)
+    _line(0,0,x,y)
+    strokeWeight(10)
+    _point(x,y)
+  })
+}
+
+drawLattice = () => {
+  stroke('black')
+  strokeWeight(8)
+  let n = 25
+  let basis = state.basis
+  for (let a0 = -n; a0 < n; a0++) {
+    for (let a1 = -n; a1 < n; a1++) {
+      let x = a0*basis[0].x + a1*basis[1].x
+      let y = a0*basis[0].y + a1*basis[1].y
+      _point(x,y)
+    }
+  }
+}
---
+++ p5/public/main.js
@@ -0,0 +1,40 @@
+
+let State = () => {
+  this.basis = [{x:-.2,y:.5},{x:.9,y:.1}]
+  this.selected =  None()
+  this.setSelected = v => this.selected = v
+  return this
+}
+
+let state = State()
+
+setup = () => {
+  createCanvas(N,N)
+}
+
+draw = () => {
+  DoIf(i => {
+    state.basis[i].x = mousePos().x
+    state.basis[i].y = mousePos().y
+  }, state.selected)
+
+  background(220)
+  drawLattice()
+  drawGrid()
+  drawBasis()
+}
+
+mousePressed = () => {
+  let {basis,setSelected} = state
+  let dists = basis
+    .map( ({x,y}) => dist(mousePos().x,mousePos().y,x,y))
+  let bound = .1
+  let close = x => x < bound
+  let findClosest = ({cd,ci},d,i) => d <= cd ? ({cd:d,ci:Some(i)}) : ({cd,ci})
+  let closest = dists.reduce(findClosest, {cd:bound,ci:None()}).ci
+  setSelected(closest)
+}
+
+mouseReleased = () => {
+  state.setSelected(None())
+}
